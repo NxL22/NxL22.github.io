3f73a52b14a80bd38daa0ce48809274ec6317ec1
neil <neilaular01@protonmail.com>
2024-01-20 00:58:42 -0300

    agregado de nuevo el curriculo con los cambios

--- script/showCV.js
+++ script/showCV.js
@@ -1,4 +1,4 @@
 // Mostrar el CV
 function mostrarPDF() {
-    window.open('https://home.mycloud.com/action/share/aef0dc14-9f59-414c-83ea-db83a0af3956', '_blank');
+    window.open('https://home.mycloud.com/action/share/fcfa4aa4-9399-42b2-afad-438cb9ed30e5', '_blank');
 }
