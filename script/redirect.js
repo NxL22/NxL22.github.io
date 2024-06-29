841c2defa02291373aa10f085b9a857f34e85d0f
neil <neilaular01@protonmail.com>
2024-06-29 19:40:11 -0400

    eliminado de la version reloj

--- script/redirect.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    if (window.innerWidth <= 200 && window.innerHeight <= 200) {
-        // Redirigir a la versión para relojes si la resolución es muy pequeña
-        window.location.href = 'reloj.html';
-    }
-});
